bee55535c77825e03e64bd4a100d96bb5cd84746
RussEfarmer <RussEfarmer@gmail.com>
2021-03-04 13:22:31 -0500

    Update badwordblocker.lua

---
+++ addons/lua/autorun/badwordblocker.lua
@@ -0,0 +1,20 @@
+badwords = {
+	"badword1",
+	"badword2",
+	"badword3",
+	"badword4",
+	"badword5" -- don't put a comma on the last one
+}
+
+function findBadWords(table, text)
+	for k,v in pairs(table) do
+		if string.find(text, v) then return true end
+	end
+end
+
+if SERVER then
+	hook.Remove("PlayerSay", "nobadword")
+	hook.Add( "PlayerSay", "nobadword", function(ply, text)
+		if findBadWords(badwords, text) then return "" end --this blocks the player message, we can make other things happen too
+	end)
+end
